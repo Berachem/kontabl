50768c9fe1326b0bfda0bbd50310030870be69c6
Berachem Markria <61350744+Berachem@users.noreply.github.com>
2022-10-25 11:33:51 +0200

    refactor: remove Uppercase begenning of name Login.php

--- api/index.php
+++ api/index.php
@@ -6,13 +6,14 @@
 include "include/Connexion.inc.php";
 
 
-/*
 switch ($_GET['action']) {
-    case 'value':
+    case 'login':
         // inclure les fichiers qui process l'action demandée
         // retourner un json contenant un clé success et un message (retourner avec les bons headers), avec le code HTTP le plus approprié
+        //break;
+        include("login.php");
         break;
-} */
+} 
 ?>
 
 <!-- test login -->
