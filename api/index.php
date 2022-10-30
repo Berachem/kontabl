86e10460e9f06c4af4ad788332ec317ea595c880
CaptainTortue <tristan.martinez@sfr.fr>
2022-10-30 12:05:34 +0100

    fix: index

--- api/index.php
+++ api/index.php
@@ -5,7 +5,7 @@
 
 include "include/Connexion.inc.php";
 
-/*
+
 switch ($_GET['action']) {
     case 'login':
         // inclure les fichiers qui process l'action demandée
@@ -29,13 +29,13 @@ switch ($_GET['action']) {
             include("transactionBySiren.php");
         } 
         break;
-} */
+}
 ?>
 
-
+<!--
 <form action="Login.php" action="insertion_form_BD.php" method="POST">
     <b>indentifiant:</b> <input type="text" name="login" required value="7745511214"/>
     <b>Mot de passe:</b><input type="text" name="password" required value="7745511214"/><br />
     <input type="reset" name="reset" value="Effacez" class = "boutton_formulaire"/> <input type="submit" name="submit" value="Validez" class = "boutton_formulaire"/>
-</form>
+</form> ->
 
