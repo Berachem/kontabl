e11eb93fadb404790793f6e47907521298195bf9
Berachem Markria <berachem.markria@gmail.com>
2022-11-09 21:51:35 +0100

    feat: pOwer Trésorerie DataTable DONE

--- api/include/Functions.inc.php
+++ api/include/Functions.inc.php
@@ -3,7 +3,7 @@
 function getSirenList() {
     global $db;
     // only if user is logged in and is a product owner
-    if (isset($_SESSION['numSiren']) && $_SESSION['type'] == 'productowner') {
+    if (isset($_SESSION['num']) && $_SESSION['type'] == 'productowner') {
         $sql = "SELECT * FROM merchant";
         $result = $db->q($sql);
         $sirenList = [];
@@ -15,7 +15,7 @@ function getSirenList() {
         // return his own siren
         $sql = "SELECT * FROM merchant WHERE idLogin = :idLogin";
         $cond = array(
-            array(":idLogin", $_SESSION['numSiren'])
+            array(":idLogin", $_SESSION['num'])
         );
         $hisSiren = $db->q($sql, $cond);
         return $hisSiren;
@@ -26,23 +26,33 @@ function getSirenList() {
 function getSocialReason(){
     global $db;
     // only if user is logged in and is a product owner
-    if (isset($_SESSION['numSiren']) && $_SESSION['type'] == 'productowner') {
+    if (isset($_SESSION['num']) && $_SESSION['type'] == 'productowner') {
         $sql = "SELECT * FROM merchant";
         $result = $db->q($sql);
         $socialReasonList = [];
         foreach ($result as $merchant) {
-            $socialReasonList[] = $merchant->name;
+            $socialReasonList[] = $merchant->raisonSociale;
         }
         return $socialReasonList;
     }else{
         // return his own social reason
         $sql = "SELECT * FROM merchant WHERE idLogin = :idLogin";
         $cond = array(
-            array(":idLogin", $_SESSION['numSiren'])
+            array(":idLogin", $_SESSION['num'])
         );
         $hisSocialReason = $db->q($sql, $cond);
-        return $hisSocialReason[0]->name;
+        return $hisSocialReason[0]->raisonSociale;
     }
+}
+
+function numSirenInDB( $numSiren){
+    global $db;
+    $sql = "SELECT * FROM merchant WHERE siren = :numSiren";
+    $cond = array(
+        array(":numSiren", $numSiren)
+    );
+    $result = $db->q($sql, $cond);
+    return count($result) > 0;
 }
 
 
--- api/pOwnerDataTable.php
+++ api/pOwnerDataTable.php
@@ -25,6 +25,8 @@ Return a JSON object with the following parameters:
 include("include/Functions.inc.php");
 
 
+
+
 function getNbTransactions($db, $numSiren, $date){
     $sql = "SELECT COUNT(*) nombre FROM transaction WHERE numSiren = :numSiren";
     $cond = array(
@@ -32,7 +34,7 @@ function getNbTransactions($db, $numSiren, $date){
     );
     if ($date){
         $sql .= " AND dateTransaction = :date";
-        $cond[] = array(":date", $date);
+        array_push($cond, array(":date", $date));
     }
     $result = $db->q($sql, $cond);
     return $result[0]->nombre;
@@ -55,6 +57,7 @@ function getMontantTotal($db, $numSiren, $date){
 }
 
 
+
 function getDevise($db, $numSiren){
     // get the currency of the merchant
     $sql = "SELECT currency FROM merchant WHERE siren = :numSiren";
@@ -65,36 +68,54 @@ function getDevise($db, $numSiren){
     return $result[0]->currency;
 }
 
-if (isset($_SESSION["id"]) && isset($_SESSION["type"])=="productowner"){ 
+if (1){  // isset($_SESSION["id"]) && isset($_SESSION["type"])=="productowner"
     // check GET parameters
     $numSiren = isset($_GET["numSiren"]) ? $_GET["numSiren"] : null;
     $raisonSociale = isset($_GET["raisonSociale"]) ? $_GET["raisonSociale"] : null;
     $date = isset($_GET["date"]) ? $_GET["date"] : null;
     $data = array();
 
-
+   
     // get the merchants
     $sql = "SELECT * FROM merchant";
     $cond = array();
-    if ($numSiren){
-        $sql .= " WHERE siren = :numSiren";
-        array_push($cond, array(":numSiren", $numSiren));
-    }
+         if ($numSiren ){
+            if (numSirenInDB($numSiren)){
+                
+                array_push($cond, array(":numSiren", $numSiren));
+            }
+            else{
+                array_push($cond, array(":numSiren", "0"));
+            }
+            $sql .= " WHERE siren = :numSiren";
+        
+        }
+
+    
     if ($raisonSociale){
         if ($numSiren){
-            $sql .= " AND name LIKE :raisonSociale";
+            $sql .= " AND raisonSociale LIKE :raisonSociale";
         } else {
-            $sql .= " WHERE name LIKE :raisonSociale";
+            $sql .= " WHERE raisonSociale LIKE :raisonSociale";
         }
         array_push($cond, array(":raisonSociale", "%".$raisonSociale."%"));
     }
     $result = $db->q($sql, $cond);
 
+    if (!$result){
+        return json_encode(array(
+            "success" => false,
+            "error" => "No merchant found"
+        ));
+        echo json_encode(array("success" => false, "error" => "No merchant found"));
+        exit();
+    }
+
     // get the data
     foreach ($result as $merchant){
         $data[] = array(
             "NumSiren" => $merchant->siren,
-            "RaisonSociale" => $merchant->name,
+            "RaisonSociale" => $merchant->raisonSociale,
             "NombreTransactions" => getNbTransactions($db, $merchant->siren, $date),
             "Devise"=> getDevise($db, $merchant->siren),
             "MontantTotal" => getMontantTotal($db, $merchant->siren, $date)
