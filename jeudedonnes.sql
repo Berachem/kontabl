990ff053c7afa138528f2664de5e518bc67909f2
CaptainTortue <tristan.martinez@sfr.fr>
2022-11-09 19:06:59 +0100

    Update jeudedonnes.sql

--- jeudedonnes.sql
+++ jeudedonnes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : dim. 30 oct. 2022 à 17:19
+-- Généré le : mer. 09 nov. 2022 à 18:03
 -- Version du serveur : 8.0.30
 -- Version de PHP : 7.4.32
 
@@ -48,7 +48,7 @@ INSERT INTO `admin` (`idAdmin`, `password`, `name`) VALUES
 
 CREATE TABLE `discount` (
   `numDiscount` int NOT NULL,
-  `numTransaction` char(6) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `numAuthorization` char(6) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `sens` char(1) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `unpaidWording` char(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   `numUnpaidFile` char(5) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
@@ -59,8 +59,8 @@ CREATE TABLE `discount` (
 -- Déchargement des données de la table `discount`
 --
 
-INSERT INTO `discount` (`numDiscount`, `numTransaction`, `sens`, `unpaidWording`, `numUnpaidFile`, `dateDiscount`) VALUES
-(1, 'abcdef', '-', 'test', '1234', NULL),
+INSERT INTO `discount` (`numDiscount`, `numAuthorization`, `sens`, `unpaidWording`, `numUnpaidFile`, `dateDiscount`) VALUES
+(1, 'abcdef', '-', 'libelle_impayer', '1234', NULL),
 (2, '123456', '+', '', '', '2022-10-03 00:00:00'),
 (3, '123456', '+', '', '', '2022-10-17 00:00:00');
 
@@ -71,11 +71,9 @@ INSERT INTO `discount` (`numDiscount`, `numTransaction`, `sens`, `unpaidWording`
 --
 
 CREATE TABLE `merchant` (
-  `name` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `raisonSociale` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `siren` char(9) NOT NULL,
-  `nbtransaction` int NOT NULL,
   `currency` varchar(3) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `totalAmount` int NOT NULL,
   `numCarte` char(16) NOT NULL,
   `network` char(2) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `password` varchar(350) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
@@ -86,9 +84,25 @@ CREATE TABLE `merchant` (
 -- Déchargement des données de la table `merchant`
 --
 
-INSERT INTO `merchant` (`name`, `siren`, `nbtransaction`, `currency`, `totalAmount`, `numCarte`, `network`, `password`, `idLogin`) VALUES
-('Leroy Merlin Noisy', '384560942', 0, 'EUR', 0, '7485', 'VS', '$2y$10$bb5LScjly5Y.YtCavNcAmOnvTYDkMm8cJqDjai.JhzynTOwyyvd4m', '7745511214'),
-('McDonald Champs sur Marne', '722003936', 0, 'EUR', 0, '1796', 'VS', '$2y$10$9pm/PQ3lnu11mo57tgjzluOr.KYJdLMEPRU5klHN6zPjgXRGx4tmK', '8755269857');
+INSERT INTO `merchant` (`raisonSociale`, `siren`, `currency`, `numCarte`, `network`, `password`, `idLogin`) VALUES
+('Leroy Merlin Noisy', '384560942', 'EUR', '7485', 'VS', '$2y$10$bb5LScjly5Y.YtCavNcAmOnvTYDkMm8cJqDjai.JhzynTOwyyvd4m', '7745511214'),
+('McDonald Champs sur Marne', '722003936', 'EUR', '1796', 'VS', '$2y$10$9pm/PQ3lnu11mo57tgjzluOr.KYJdLMEPRU5klHN6zPjgXRGx4tmK', '8755269857');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `merchant_temp`
+--
+
+CREATE TABLE `merchant_temp` (
+  `raisonSociale` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `siren` char(9) NOT NULL,
+  `currency` varchar(3) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `numCarte` char(16) NOT NULL,
+  `network` char(2) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `password` varchar(350) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `idLogin` varchar(350) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -116,22 +130,23 @@ INSERT INTO `productowner` (`idProductowner`, `name`, `password`) VALUES
 --
 
 CREATE TABLE `transaction` (
+  `idTransaction` int NOT NULL,
   `numAuthorization` char(6) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `dateTransaction` datetime NOT NULL,
   `endingFoursCardNumbers` char(4) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `amount` int NOT NULL,
-  `currency` varchar(3) NOT NULL,
-  `numSiren` char(9) NOT NULL
+  `currency` varchar(3) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `numSiren` char(9) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `amount` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `transaction`
 --
 
-INSERT INTO `transaction` (`numAuthorization`, `dateTransaction`, `endingFoursCardNumbers`, `amount`, `currency`, `numSiren`) VALUES
-('123456', '2022-09-14 00:00:00', '1245', 100000, 'EUR', '384560942'),
-('1542za', '2017-10-26 00:00:00', '1488', 500, 'EUR', '722003936'),
-('abcdef', '2022-10-10 00:00:00', '1458', 200000, 'EUR', '722003936');
+INSERT INTO `transaction` (`idTransaction`, `numAuthorization`, `dateTransaction`, `endingFoursCardNumbers`, `currency`, `numSiren`, `amount`) VALUES
+(1, '123456', '2022-09-14 00:00:00', '1245', 'EUR', '384560942', 0),
+(2, '1542za', '2017-10-26 00:00:00', '1488', 'EUR', '722003936', 0),
+(3, 'abcdef', '2022-10-10 00:00:00', '1458', 'EUR', '722003936', 0);
 
 --
 -- Index pour les tables déchargées
@@ -148,12 +163,18 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `discount`
   ADD PRIMARY KEY (`numDiscount`),
-  ADD KEY `transaction` (`numTransaction`);
+  ADD KEY `transaction` (`numAuthorization`);
 
 --
 -- Index pour la table `merchant`
 --
 ALTER TABLE `merchant`
+  ADD PRIMARY KEY (`siren`);
+
+--
+-- Index pour la table `merchant_temp`
+--
+ALTER TABLE `merchant_temp`
   ADD PRIMARY KEY (`siren`);
 
 --
@@ -166,7 +187,8 @@ ALTER TABLE `productowner`
 -- Index pour la table `transaction`
 --
 ALTER TABLE `transaction`
-  ADD PRIMARY KEY (`numAuthorization`),
+  ADD PRIMARY KEY (`idTransaction`),
+  ADD UNIQUE KEY `numAuthorization` (`numAuthorization`),
   ADD KEY `siren` (`numSiren`);
 
 --
@@ -180,6 +202,12 @@ ALTER TABLE `discount`
   MODIFY `numDiscount` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
+-- AUTO_INCREMENT pour la table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `idTransaction` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- Contraintes pour les tables déchargées
 --
 
@@ -187,7 +215,7 @@ ALTER TABLE `discount`
 -- Contraintes pour la table `discount`
 --
 ALTER TABLE `discount`
-  ADD CONSTRAINT `transaction` FOREIGN KEY (`numTransaction`) REFERENCES `transaction` (`numAuthorization`) ON DELETE RESTRICT ON UPDATE CASCADE;
+  ADD CONSTRAINT `transaction` FOREIGN KEY (`numAuthorization`) REFERENCES `transaction` (`numAuthorization`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `transaction`
