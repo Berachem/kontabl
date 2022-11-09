caaa19309201da2dd2b00f6b8b1601762a698189
Berachem <berachem.markria@gmail.com>
2022-11-09 12:08:42 +0100

    feat: Account Creation

---
+++ api/adminAskCreateAccount.php
@@ -0,0 +1,121 @@
+<?php
+/* 
+POST 
+ - idLogin
+ - password`
+GET:
+    - `raisonSociale` ,
+    - `siren` ,
+    - `currency`, 
+    - `numCarte`,
+    - `network` ,
+
+add a temporary merchant account
+
+*/
+
+include("include/Functions.inc.php");
+session_start();
+
+
+
+// check if the user is admin
+if (isset($_SESSION['type']) && $_SESSION['type']== "admin"){
+
+    // Check if all the parameters are set
+    if (isset($_GET["raisonSociale"]) && isset($_GET["siren"]) && isset($_GET["currency"]) && isset($_GET["numCarte"]) && isset($_GET["network"]) 
+    && isset($_POST["idLogin"]) && isset($_POST["password"])){
+
+        // Check if the parameters are not empty
+        if (!empty($_GET["raisonSociale"]) && !empty($_GET["siren"]) && !empty($_GET["currency"]) && !empty($_GET["numCarte"]) && !empty($_GET["network"])
+        && !empty($_POST["idLogin"]) && !empty($_POST["password"])){
+
+            // Check if the merchant already exists
+            $param = array(
+                array(':idLogin',$_GET['idLogin'],PDO::PARAM_STR),
+            );
+            $merchant = $db -> q("SELECT * FROM merchant WHERE idLogin = :idLogin;", $param); 
+            if ($merchant){
+                $response = [
+                    "success" => false,
+                    "error" => "merchant already exists"
+                ]; 
+            }
+            else{
+                // Check if the merchant is already in the temporary table
+                $param = array(
+                    array(':idLogin',$_GET['idLogin'],PDO::PARAM_STR),
+                );
+                $merchant = $db -> q("SELECT * FROM merchant_temp WHERE idLogin = :idLogin;", $param); 
+                if ($merchant){
+                    $response = [
+                        "success" => false,
+                        "error" => "merchant already exists in the temporary table"
+                    ]; 
+                }
+
+                // Check if the siren already exists
+                $param = array(
+                    array(':siren',$_GET['siren'],PDO::PARAM_STR),
+                );
+                $merchant = $db -> q("SELECT * FROM merchant WHERE siren = :siren;", $param);
+
+                if ($merchant){
+                     // return error
+                    $response = [
+                        "success" => false,
+                        "error" => "siren already exists"
+                    ];
+                }
+                else{
+                    // check if the siren already exists in the temporary table
+                    $param = array(
+                        array(':siren',$_GET['siren'],PDO::PARAM_STR),
+                    );
+                    $merchant = $db -> q("SELECT * FROM merchant_temp WHERE siren = :siren;", $param);
+
+                    if ($merchant){
+                        // return error
+                        $response = [
+                            "success" => false,
+                            "error" => "siren already exists in the temporary table"
+                        ];
+
+                    }
+
+                }
+
+                // if response is not set
+                if (!isset($response)){
+                    // add the merchant to the temporary table
+                    $param = array(
+                        array(':raisonSociale',$_GET['raisonSociale'],PDO::PARAM_STR),
+                        array(':siren',$_GET['siren'],PDO::PARAM_STR),
+                        array(':currency',$_GET['currency'],PDO::PARAM_STR),
+                        array(':numCarte',$_GET['numCarte'],PDO::PARAM_STR),
+                        array(':network',$_GET['network'],PDO::PARAM_STR),
+                        array(':password',$_GET['password'],PDO::PARAM_STR),
+                        array(':idLogin',$_GET['idLogin'],PDO::PARAM_STR),
+                    );
+                    $merchant = $db -> q("INSERT INTO merchant_temp (raisonSociale, siren, currency, numCarte, network, password, idLogin) VALUES (:raisonSociale, :siren, :currency, :numCarte, :network, :password, :idLogin);", $param);
+
+                    // return success
+                    $response = [
+                        "success" => true,
+                        "numSiren" => $_GET['siren']
+                    ];
+                }
+
+
+            }
+
+        
+
+}}}
+
+
+
+
+
+
+?>
--- api/pOwnerAcceptAccountCreation.php
+++ api/pOwnerAcceptAccountCreation.php
@@ -1,6 +1,57 @@
 <?php
 
-/* A FAIRE */
+/*
+GET:
+-numSiren (must) in table merchant_temp
+
+*/
+
+// check if it is the product owner
+
+if (isset($_SESSION['type']) && $_SESSION['type'] == 'productowner' && isset($_GET["numSiren"])) {
+    
+    // transfer the merchant from merchant_temp to merchant
+    // merchant : name, siren, currency, numCarte, network,password, idLogin 
+
+    $param = array(
+        array(':siren', $_GET['numSiren'], PDO::PARAM_STR),
+    );
+    $merchant = $db->q("SELECT * FROM merchant_temp WHERE siren = :siren;", $param);
+
+    if ($merchant) {
+        // insert the merchant in the merchant table
+        $param = array(
+            array(':name', $merchant[0]['name'], PDO::PARAM_STR),
+            array(':siren', $merchant[0]['siren'], PDO::PARAM_STR),
+            array(':currency', $merchant[0]['currency'], PDO::PARAM_STR),
+            array(':numCarte', $merchant[0]['numCarte'], PDO::PARAM_STR),
+            array(':network', $merchant[0]['network'], PDO::PARAM_STR),
+            array(':password', $merchant[0]['password'], PDO::PARAM_STR),
+            array(':idLogin', $merchant[0]['idLogin'], PDO::PARAM_STR),
+        );
+        $db->q("INSERT INTO merchant (name, siren, currency, numCarte, network, password, idLogin) VALUES (:name, :siren, :currency, :numCarte, :network, :password, :idLogin);", $param);
+
+        // delete the merchant from the merchant_temp table
+        $param = array(
+            array(':siren', $_GET['numSiren'], PDO::PARAM_STR),
+        );
+        $db->q("DELETE FROM merchant_temp WHERE siren = :siren;", $param);
+
+        $response = [
+            "success" => true,
+            "numSiren" => $_GET['numSiren']
+        ];
+    } else {
+        $response = [
+            "success" => false,
+            "error" => "merchant does not exist in the temporary table"
+        ];
+    }
+}
+
+
+
+
 
 
 
