0ec8eaf4c1ec81a5eb6516e5a464571f694bdd60
Berachem Markria <berachem.markria@gmail.com>
2022-11-10 19:10:31 +0100

    ref: index back

--- api/index.php
+++ api/index.php
@@ -5,42 +5,40 @@
 
 include "include/Connexion.inc.php";
 
+
+
+
 if (isset($_GET['action'])){
     switch ($_GET['action']) {
         case 'login':
             // inclure les fichiers qui process l'action demandée
             // retourner un json contenant un clé success et un message (retourner avec les bons headers), avec le code HTTP le plus approprié
             //break;
-            include("login.php");
+            include("/access/login.php");
             break;
         case 'logout':
-            include("logout.php");
+            include("/access/logout.php");
             break;
         case 'isLoggedIn':
-            include("isLoggedIn.php");
-            break;
-        case 'transactionById':
-            if (isset($_GET['transactionId'])) {
-                include("transactionById.php");
-            } 
-            break;
-        case 'clientDataTable':
-            if (isset($_GET['NumSiren'])) {
-                include("clientDataTable.php");
-            } 
-            break;
-        case "discountDataTable":
-            include("discountDataTable.php");
+            include("/access/isLoggedIn.php");
             break;
         case "treasuryDataTable":
-            include("treasuryDataTable.php");
+                include("/treasury/treasuryDataTable.php");
+            break;
+        case "discountDataTable":
+                include("/discount/discountDataTable.php");
             break;
         case "unPaidDiscountDataTable":
-            include("unPaidDiscountDataTable.php");
+                include("/discount/unPaidDiscountDataTable.php");
             break;
+        
+        
     }
 }
 
 
 ?>
+
+
+
 
